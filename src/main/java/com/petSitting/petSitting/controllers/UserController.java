54d6507402bac55305eb5a79637062abdfc5d504
= <=>
2025-01-09 19:07:42 +0530

    user controller done

--- src/main/java/com/petSitting/petSitting/controllers/UserController.java
+++ src/main/java/com/petSitting/petSitting/controllers/UserController.java
@@ -1,4 +1,48 @@
 package com.petSitting.petSitting.controllers;
 
+
+import com.petSitting.petSitting.models.User;
+import com.petSitting.petSitting.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) throws Exception {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    // Update user
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        User updatedUser = userService.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
+
+
+
+
 }
--- src/main/java/com/petSitting/petSitting/service/UserService.java
+++ src/main/java/com/petSitting/petSitting/service/UserService.java
@@ -58,4 +58,6 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+
+
 }
